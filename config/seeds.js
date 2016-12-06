7c4a6cbd6c688f8985d6014331a16f61f77f4538
L. Guo <iml885203@gmail.com>
2016-12-06 23:02:40 +0800

    fix sails seed

--- config/seeds.js
+++ config/seeds.js
@@ -9,39 +9,36 @@
  */
 
 module.exports.seeds = {
-  user: {
-    data: [
-      {
-        account: 'admin',
-        name: '大中天',
-        password: 'password',
-        permission: 'SuperAdmin',
-      },
-      {
-        account: 'user1',
-        name: '攻城屍1',
-        password: 'password',
-        permission: 'user',
-      },
-      {
-        account: 'user2',
-        name: '攻城屍2',
-        password: 'password',
-        permission: 'user',
-      },
-      {
-        account: 'user3',
-        name: '攻城屍3',
-        password: 'password',
-        permission: 'user',
-      },
-      {
-        account: 'user4',
-        name: '攻城屍4',
-        password: 'password',
-        permission: 'user',
-      },
-    ],
-    overwrite: false
-  },
+  user: [
+    {
+      account: 'admin',
+      name: '大中天',
+      password: 'password',
+      permission: 'SuperAdmin',
+    },
+    {
+      account: 'user1',
+      name: '攻城屍1',
+      password: 'password',
+      permission: 'user',
+    },
+    {
+      account: 'user2',
+      name: '攻城屍2',
+      password: 'password',
+      permission: 'user',
+    },
+    {
+      account: 'user3',
+      name: '攻城屍3',
+      password: 'password',
+      permission: 'user',
+    },
+    {
+      account: 'user4',
+      name: '攻城屍4',
+      password: 'password',
+      permission: 'user',
+    },
+  ],
 }
