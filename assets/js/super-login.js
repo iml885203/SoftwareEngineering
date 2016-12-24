fcd98b89bd25ec530335a026a56c70298d2f22a0
L. Guo <iml885203@gmail.com>
2016-12-24 15:00:33 +0800

    fix super-login button name is username

--- assets/js/super-login.js
+++ assets/js/super-login.js
@@ -3,7 +3,7 @@ $(function(){
   $('.js-super-login').each(function(){
     let self = this;
     $(self).click(function(){
-      let account = $(this).text().trim();
+      let account = $(this).data('account');
       $('input[name="account"]').val(account);
       $('input[name="password"]').val('password');
       $('form[action="login"]').submit();
